dd16f199fa34da9c4e9f3d4c9d8fe323e1c60a96
daria.dsg <daria.ui.ux@gmail.com>
2022-10-10 12:47:14 +0200

    Board raise errors and finalize the setting up

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-require "./pieces/piece.rb"
+require_relative "pieces.rb"
 require "byebug"
 
 class Board
@@ -7,16 +7,35 @@ class Board
     end
 
     def [](pos)
+        raise "Invalid position" unless valid_pos?(pos)
+
         x, y = pos
         @rows[x][y]
     end
 
     def []=(pos,val)
+        raise "Invalid position" unless valid_pos?(pos)
+
         x, y = pos
         @rows[x][y] = val
     end
 
+    def move_piece(start_pos, end_pos)
+        raise "start position is empty" if empty?(start_pos)
+        self[end_pos], self[start_pos] = self[start_pos], nil
+    end
+
+    def valid_pos?(pos)
+        x, y = pos
+        x.between?(0,8) && y.between?(0,8)
+    end
+
     def add_piece(piece,pos)
+        raise "position is not empty" unless empty?(pos)
         self[pos] = piece
+    end
+
+    def empty?(pos)
+        self[pos] == nil
     end
 end
--- pieces/piece.rb
+++ pieces/piece.rb
@@ -4,6 +4,10 @@ class Piece
     def intitialize(color, board, pos)
         @color, @board, @pos = color, board, pos
     end
+
+    def empty?
+        self == nil
+    end
 end
 
 
