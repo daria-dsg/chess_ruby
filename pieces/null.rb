e28f428c9a5ac0425df51f982690b77082064c44
daria.dsg <daria.ui.ux@gmail.com>
2022-10-20 19:23:47 +0200

    Implement null piece

--- pieces/null.rb
+++ pieces/null.rb
@@ -1,9 +1,19 @@
 require_relative "piece"
+require_relative "singleton"
 
 class Null < Piece
+    include Singleton
+
+    def initialize
+        @color = :none
+    end
 
     def symbol
-        " "
+        ""
+    end
+
+    def moves
+        []
     end
 
 end
