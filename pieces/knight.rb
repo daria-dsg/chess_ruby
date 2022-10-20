e5489ff1da4d7e0ff45cae76593805c049788112
daria.dsg <daria.ui.ux@gmail.com>
2022-10-20 15:58:38 +0200

    Develop moves function for knight

--- pieces/knight.rb
+++ pieces/knight.rb
@@ -1,9 +1,24 @@
 require_relative "piece.rb"
 
 class Knight < Piece
-
+    include Stepable
+    
     def symbol
         "♘".colorize(color)
     end
 
+    protected 
+
+    def move_diffs
+        [
+            [-1, -2],
+            [-2, -1],
+            [1, -2],
+            [2, -1],
+            [2, 1],
+            [1, 2],
+            [-1, 2],
+            [-2, 1],
+        ]
+    end
 end
--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -1,19 +1,9 @@
+require "byebug"
+
 module Stepable
+
     def moves
-        moves = []
-        x, y = pos
-        (-1 .. 1).each do |i|
-            (-1 .. 1).each do |j|
-                possible_move = [x + i, y + j]
-                next if edge?(possible_move) || possible_move == pos
-                moves << possible_move
-            end
-        end
-
-        moves
+        move_diffs.map { |diff| [diff, pos].transpose.map(&:sum)}
     end
 
-    def edge?(pos)
-        (pos[0] < 0 || pos[0] > 7) || (pos[1] < 0 || pos[1] > 7)
-    end
 end
