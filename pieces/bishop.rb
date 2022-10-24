65569a73ba537721d0c527fef6ccb93e28cb71eb
daria.dsg <daria.ui.ux@gmail.com>
2022-10-24 07:54:00 +0200

    Format bishop class

--- pieces/bishop.rb
+++ pieces/bishop.rb
@@ -2,16 +2,15 @@ require_relative 'piece'
 require_relative 'slideable'
 
 class Bishop < Piece
-    include Slideable
+  include Slideable
 
-    def symbol
-        "♗".colorize(color)
-    end
+  def symbol
+    "♗".colorize(color)
+  end
 
-    private
-    
-    def move_dirs
-        diagonal_dirs
-    end
+  private
 
+  def move_dirs
+    diagonal_dirs
+  end
 end
